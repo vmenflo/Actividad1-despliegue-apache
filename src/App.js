c56e8b7113f1432eab0d1f42743e5a89184566fd
Víctor Mena <menaflores.victor@gmail.com>
2025-02-20 18:01:38 +0100

    Cambios en App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           className='freecodecamp-logo' />
       </div>
       <div className='tareas-lista-principal'>
-        <h1>Mis Tareas</h1>
+        <h1>Actividad Victor Mena Flores</h1>
         <ListaDeTareas />
       </div>
     </div>
